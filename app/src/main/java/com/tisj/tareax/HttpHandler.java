51fdc213eaf74c1f4eaa4dfc351638f22bb7eb4f
Diego Mascheroni <maskeee@gmail.com>
2016-10-28 16:46:28 -0300

    Login via POST

--- app/src/main/java/com/tisj/tareax/HttpHandler.java
+++ app/src/main/java/com/tisj/tareax/HttpHandler.java
@@ -1,16 +1,28 @@
 package com.tisj.tareax;
 
+import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,12 +43,12 @@ public class HttpHandler {
             URL url = new URL(reqUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod(method);
-            if (method == "POST") {
-//            for(Map.Entry<String, String> param : parametros){
+//            if (method == "POST") {
+//
+//            }            for(Map.Entry<String, String> param : parametros){
 //
 //            }
-//            conn.set
-            }
+//            conn.
 
 
             // read the response
@@ -52,6 +64,50 @@ public class HttpHandler {
             Log.e(TAG, "Exception: " + e.getMessage());
         }
         return response;
+    }
+
+    public boolean getLogin(String reqUrl, String method, String cedula, String pass) {
+
+        String response = null;
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setDoInput(true);
+            conn.setRequestMethod(method);
+
+            Uri.Builder builder = new Uri.Builder()
+                    .appendQueryParameter("cedula", cedula)
+                    .appendQueryParameter("password", pass);
+            String query = builder.build().getEncodedQuery();
+
+            conn.connect();
+            OutputStream os = new BufferedOutputStream(conn.getOutputStream());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+
+            writer.write(query);
+            writer.flush();
+            writer.close();
+
+
+            return conn.getResponseCode() == 200;
+            // read the response
+//            InputStream in = new BufferedInputStream(conn.getInputStream());
+//
+//
+//            response = convertStreamToString(in);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage());
+        } catch (ProtocolException e) {
+            Log.e(TAG, "ProtocolException: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+
+        return false;
     }
 
     private String convertStreamToString(InputStream is) {
@@ -74,4 +130,6 @@ public class HttpHandler {
         }
         return sb.toString();
     }
+
+
 }
--- app/src/main/java/com/tisj/tareax/LoginActivity.java
+++ app/src/main/java/com/tisj/tareax/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.tisj.tareax;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
@@ -7,11 +8,20 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,8 +29,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button btnCheckConexion;
     Button btnSinLogin;
 
-    EditText user;
-    EditText pass;
+    public EditText user;
+    public EditText pass;
+    boolean login = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +56,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Intent intento;
         switch (view.getId()){
             case R.id.login:
-                String usuario = user.getText().toString();
-                Toast.makeText(getApplicationContext(),usuario,Toast.LENGTH_SHORT).show();
+                String cedula = user.getText().toString();
+                String password = pass.getText().toString();
+
+                new obtenerLogin().execute();
+
                 break;
             case R.id.iniciarSinLogin:
                 intento = new Intent(this.getApplicationContext(), ListarEstudiantesActivity.class);
@@ -55,28 +69,50 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.chequearConexion:
                 TextView conexionText = (TextView)findViewById(R.id.conexionTxt);
 
-                ConnectivityManager connMgr = (ConnectivityManager)
-                        getSystemService(Context.CONNECTIVITY_SERVICE);
+//                ConnectivityManager connMgr = (ConnectivityManager)
+//                        getSystemService(Context.CONNECTIVITY_SERVICE);
+//
+//                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+//                if (networkInfo != null && networkInfo.isConnected()) {
+//                    conexionText.setText("Hay Conexión :)");
+//                } else {
+//                    conexionText.setText("No hay Conexión :(");
+//                }
 
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                if (networkInfo != null && networkInfo.isConnected()) {
-                    conexionText.setText("Hay Conexión :)");
-                } else {
-                    conexionText.setText("No hay Conexión :(");
-                }
+
+
+
                 break;
         }
     }
 
     private class obtenerLogin extends AsyncTask<String,Void,Void> {
 
-        String resultado;
+
+        String cedula = user.getText().toString();
+        String password = pass.getText().toString();
+        //boolean login;
 
         @Override
         protected Void doInBackground(String... strings) {
 
+            HttpHandler sh = new HttpHandler();
+            login = sh.getLogin("http://diegonicolas.webcindario.com/Login.php", "POST", cedula, password);
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Intent intento;
+            if(login){
+                Toast.makeText(getApplicationContext(), "Login exitoso", Toast.LENGTH_SHORT).show();
+                intento = new Intent(getApplicationContext(), ListarEstudiantesActivity.class);
+                startActivity(intento);
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Login incorrecto", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
